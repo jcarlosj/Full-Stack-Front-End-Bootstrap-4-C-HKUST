fcf6b28495eee8836a2c8ca52286eda2901584b3
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-07-10 18:20:35 -0500

    [+] Grunt: Define task to convert a file with Sass code in CSS

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,34 @@
 'use strict';
 
 module .exports = function( grunt ) {
+    /** Dependencies */
+    require( 'time-grunt' )( grunt );        // Display the elapsed execution time of grunt tasks
+    require( 'jit-grunt' )( grunt );
+    const sass = require( 'node-sass' );
+
     // Project configuration.
-    console .log( 'Gruntfile.js' );
-    grunt .initConfig({});      // Inicializa configuración
+    grunt .initConfig({
+        /** Convert Sass code to CSS */
+        sass: {                                                     // Task
+            dist: {                                                 // Target
+                options: {                                          // Target Options
+                    implementation: sass,
+                    sourceMap: true,
+                },
+                files: {                                            // Dictionary of files
+                    './dist/css/styles.css': './css/master.scss'    // 'destination': 'source'
+                }
+            }
+        }
+    });
+
+    /** Load the plugins that provides the tasks */
+    grunt .loadNpmTasks( 'grunt-sass' );                        
+
+    /** Register Tasks */
+    grunt .registerTask( 'css', [ 'sass' ] );
+
+    /** Default task(s). */ 
+    grunt .registerTask( 'default', [] );
+
 }
