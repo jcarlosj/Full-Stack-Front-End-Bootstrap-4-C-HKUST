11a872d93005240ecacf2eb1d71beec3e3f460d6
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-07-10 18:52:13 -0500

    [+] Grunt: Define task to create watch and server tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,6 +8,31 @@ module .exports = function( grunt ) {
 
     // Project configuration.
     grunt .initConfig({
+        /** Run predefined tasks whenever observed file patterns are added, modified, or deleted */
+        watch: {                                                    // Task
+            scripts: {
+                files: [ './css/*.scss' ],
+                tasks: [ 'sass' ]
+            },
+        },
+        /** Browser-sync module */
+        browserSync: {
+            dev: {
+                bdFiles: {
+                    src: [
+                        './css/*.css',
+                        './js/*.js',
+                        '*.html'
+                    ]
+                },
+                options: {
+                    watchTask: true,
+                    server: {
+                        baseDir: './'
+                    }
+                }
+            }
+        },
         /** Convert Sass code to CSS */
         sass: {                                                     // Task
             dist: {                                                 // Target
@@ -16,19 +41,21 @@ module .exports = function( grunt ) {
                     sourceMap: true,
                 },
                 files: {                                            // Dictionary of files
-                    './dist/css/styles.css': './css/master.scss'    // 'destination': 'source'
+                    './css/master.css': './css/master.scss'         // 'destination': 'source'
                 }
             }
         }
     });
 
     /** Load the plugins that provides the tasks */
-    grunt .loadNpmTasks( 'grunt-sass' );                        
+    grunt .loadNpmTasks( 'grunt-sass' );    
+    grunt .loadNpmTasks( 'grunt-contrib-watch' );    
+    grunt .loadNpmTasks( 'grunt-browser-sync' );                    
 
     /** Register Tasks */
     grunt .registerTask( 'css', [ 'sass' ] );
 
     /** Default task(s). */ 
-    grunt .registerTask( 'default', [] );
+    grunt .registerTask( 'default', [ 'browserSync', 'watch' ] );
 
 }
