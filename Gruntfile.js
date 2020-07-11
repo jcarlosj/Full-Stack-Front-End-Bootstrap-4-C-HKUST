0f1a2d731dd1a7e249d62db4cd07b0b784d7819c
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-07-11 11:33:22 -0500

    [+] Grunt: Building the Distribution and Files

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,10 @@
 module .exports = function( grunt ) {
     /** Dependencies */
     require( 'time-grunt' )( grunt );        // Display the elapsed execution time of grunt tasks
-    require( 'jit-grunt' )( grunt );
+    require( 'jit-grunt' )( grunt, {
+        useminPrepare: 'grunt-usemin'
+    } );
+
     const sass = require( 'node-sass' );
 
     // Project configuration.
@@ -86,6 +89,88 @@ module .exports = function( grunt ) {
                     dest: './dist/'                                 // Destination
                 }]
             }
+        },
+        /** useminPrepare */
+        useminPrepare: {
+            foo: {
+                src: [ 'aboutus.html', 'contactus.html', 'index.html' ],
+                dest: './dist'
+            },
+            options: {
+                flow: {
+                    steps: {
+                        css: [ 'cssmin' ],
+                        js: [ 'uglify' ]
+                    },
+                    post: {
+                        css: [{
+                            name: 'cssmin',
+                            createConfig: function( context, block ) {
+                                var generated = context .options .generated;
+
+                                generated .options = {
+                                    keepSpecialComments: 0,
+                                    rebase: false
+                                }
+                            }
+                        }]
+                    }
+                }
+            }
+        },
+        /** Concatenate files. */
+        concat: {
+            options: {
+                separator: ';',
+            },
+            // dist configuration is provided by useminPrepare
+            dist: {},
+        },
+        /** Minify JavaScript files with UglifyJS */
+        uglify: {
+            // dist configuration is provided by useminPrepare
+            dist: {}
+        },
+        /** Minify CSS */
+        cssmin: {
+            // dist configuration is provided by useminPrepare
+            dist: {}
+        },
+        /** Static asset revisioning through file content hash */
+        filerev: {
+            options: {
+                encoding: 'utf8',
+                algorithm: 'md5',
+                length: 20
+            },
+            // filerev:files hashes(md5) all assets (images, js and css ) in dist directory
+            files: {
+                src: [
+                    './dist/css/*.css',
+                    './dist/js/*.js'
+                ]
+            }  
+        },
+        /** Replaces references from non-optimized scripts, stylesheets and other assets to their optimized version within a set of HTML files (or any templates/views). */
+        usemin: {
+            html: [ './dist/aboutus.html', './dist/contactus.html', './dist/index.html' ],
+            options: {
+                assestsDirs: [ './dist', './dist/css', './dist/js' ]    // options.assetDirs contains the directories for finding the assets according to their relative paths
+            }
+        },
+        /** Minify HTML */
+        htmlmin: {                                          // Task
+            dist: {                                         // Target
+                options: {                                  // Target options
+                    removeComments: true,
+                    collapseWhitespace: true
+                },
+                files: {                                                 // Dictionary of files
+                    './dist/aboutus.html': './dist/aboutus.html',        // 'destination': 'source'
+                    './dist/contactus.html': './dist/contactus.html',    // 'destination': 'source'
+                    './dist/index.html': './dist/index.html'             // 'destination': 'source'
+                }
+            }
         }
     });
 
@@ -95,11 +180,11 @@ module .exports = function( grunt ) {
     grunt .loadNpmTasks( 'grunt-browser-sync' );   
     grunt .loadNpmTasks( 'grunt-contrib-copy' );        
     grunt .loadNpmTasks( 'grunt-contrib-clean' );  
-    grunt .loadNpmTasks( 'grunt-contrib-imagemin' );         
+    grunt .loadNpmTasks( 'grunt-contrib-imagemin' );     
 
     /** Register Tasks */
     grunt .registerTask( 'css', [ 'sass' ] );
-    grunt .registerTask( 'build', [ 'clean:build', 'copy', 'imagemin' ] );
+    grunt .registerTask( 'build', [ 'clean:build', 'copy', 'imagemin', 'useminPrepare', 'concat', 'cssmin', 'uglify', 'filerev', 'usemin', 'htmlmin' ] );
 
     /** Default task(s). */ 
     grunt .registerTask( 'default', [ 'browserSync', 'watch' ] );
