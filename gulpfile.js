f6063f9dde859000006d6acd22caa8e4972afb1d
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-07-12 11:48:47 -0500

    [+] Gulp: 1. Define the task to delete the dist directory. 2. Define the tasks for copying html files and fonts.

--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,27 @@
 var
     { src, dest, watch, series } = require( 'gulp' ),
     sass = require( 'gulp-sass' ),
-    browserSync = require( 'browser-sync' ) .create();
+    browserSync = require( 'browser-sync' ) .create(),
+    del = require( 'del' );
 
 /** Tasks */
+function cleanDistTask( cb ) {
+    del .sync([ './dist' ]);
+    cb();
+}
+
+function copyFontsTask( cb ) {
+    src( './node_modules/font-awesome/fonts/**/*.{ttf,woff,woff2,eot,otf,svg}' )
+        .pipe( dest( './dist/fonts' ) );
+    cb();
+}
+
+function copyFilesTask( cb ) {
+    src( './src/*.html' )
+        .pipe( dest( './dist' ) );
+    cb();
+}
+
 sass .compiler = require( 'node-sass' );
 function sassTask( cb ) { 
     src( './src/css/*.scss' )
@@ -39,3 +57,4 @@ function browserSyncTask( cb ) {
 
 exports .default = series( browserSyncTask, sassWatchTask );
 exports .sass = sassTask;
+exports .copy = series( copyFontsTask, copyFilesTask );
