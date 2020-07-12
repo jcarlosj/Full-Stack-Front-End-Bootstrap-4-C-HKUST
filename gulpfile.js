99826ffb8f88edb2ddfecb001bfc1b9d1ee2a3eb
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-07-12 13:20:37 -0500

    [+] Gulp: Building the Distribution and Files

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,19 @@
 
 /** Dependencies */
 var
-    { src, dest, watch, series } = require( 'gulp' ),
+    { src, dest, watch, series, parallel } = require( 'gulp' ),
     sass = require( 'gulp-sass' ),
     browserSync = require( 'browser-sync' ) .create(),
     del = require( 'del' ),
-    imagemin = require( 'gulp-imagemin' );
+    imagemin = require( 'gulp-imagemin' ),
+    uglify = require( 'gulp-uglify' ),
+    usemin = require( 'gulp-usemin' ),
+    rev = require( 'gulp-rev' ),
+    cleanCss = require( 'gulp-clean-css' ),
+    flatmap = require( 'gulp-flatmap' ),
+    htmlmin = require( 'gulp-htmlmin' );
 
-/** Tasks */
+/** Clean files and folders */
 function cleanDistTask( cb ) {
     del .sync([ './dist' ]);
     cb();
@@ -20,12 +26,14 @@ function copyFontsTask( cb ) {
     cb();
 }
 
+/** Copy files and folders */
 function copyFilesTask( cb ) {
     src( './src/*.html' )
         .pipe( dest( './dist' ) );
     cb();
 }
 
+/** Minify images */
 function compressImagesTask( cb ) {
     src( './src/img/*' )
         .pipe( imagemin({
@@ -37,6 +45,7 @@ function compressImagesTask( cb ) {
     cb();
 }
 
+/** Convert Sass code to CSS */
 sass .compiler = require( 'node-sass' );
 function sassTask( cb ) { 
     src( './src/css/*.scss' )
@@ -45,11 +54,29 @@ function sassTask( cb ) {
     cb();
 }
 
+/** Run predefined tasks whenever observed file patterns are added, modified, or deleted */
 function sassWatchTask( cb ) {
     watch( './src/css/*.scss', sassTask );
     cb();
 }
 
+/** Replaces references from non-optimized scripts, stylesheets and other assets to their optimized version within a set of HTML files (or any templates/views). */
+function useMinTask( cb ) {
+    src( './src/*.html' )
+        .pipe( flatmap( function( stream, file ) {
+            return stream .pipe( usemin({       
+                html: [ htmlmin({ collapseWhitespace: true }) ],        
+                css: [ rev() ],                                         /** Minify CSS */
+                js: [ uglify(), rev() ],                                /** Minify JavaScript files with UglifyJS */
+                inlinejs: [ uglify() ],
+                inlinecss: [ cleanCss(), 'concat' ]                     /** Minify HTML */
+            }))
+        }))
+        .pipe( dest( './dist' ) );
+    cb();
+}
+
+/** Browser-sync module */
 function browserSyncTask( cb ) {
     var files = [
         '*.html',
@@ -68,4 +95,5 @@ function browserSyncTask( cb ) {
 }
 
 exports .default = series( browserSyncTask, sassWatchTask );
-exports .sass = sassTask;
+exports .build = series( cleanDistTask, parallel( copyFontsTask, compressImagesTask, useMinTask ) );
+exports .clear = cleanDistTask;
