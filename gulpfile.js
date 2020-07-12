eb0d25afbed72f079307dd6a0e9438745fa1ef3d
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-07-12 10:49:40 -0500

    [+] Gulp: Define task to convert a file with Sass code in CSS

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,23 @@
-function defaultTask(cb) {
-  // place code for your default task here
-  cb();
+'use strict';
+
+/** Dependencies */
+var
+    { src, dest } = require( 'gulp' ),
+    sass = require( 'gulp-sass' );
+
+/** Tasks */
+sass .compiler = require( 'node-sass' );
+function sassTask( cb ) { 
+    src( './src/css/*.scss' )
+        .pipe( sass() .on( 'error', sass .logError ) )
+        .pipe( dest( './dist/css' ) );
+    cb();
+}
+
+function defaultTask( cb ) {
+    // place code for your default task here
+    cb();
 }
 
 exports .default = defaultTask;
+exports .sass = sassTask;
